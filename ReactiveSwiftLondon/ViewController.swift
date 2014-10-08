77c0dcae01b8e1dc317bd2ff7ea2b90b858a361b
Colin Eberhardt <colin.eberhardt@gmail.com>
2014-10-08 21:47:19 +0100

    Added ReactiveCocoa and a simple reactive text field

--- ReactiveSwiftLondon/ViewController.swift
+++ ReactiveSwiftLondon/ViewController.swift
@@ -8,18 +8,31 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
 
+  @IBOutlet weak var searchTextField: UITextField!
+  @IBOutlet weak var tweetsTableView: UITableView!
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+   
+    tweetsTableView.dataSource = self
+    
+    searchTextField.rac_textSignal().subscribeNextAs {
+      (text: String) in
+      println(text)
+    }
   }
 
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
+  func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return 10
   }
-
+  
+  func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCellWithIdentifier("Cell") as UITableViewCell
+    
+    return cell
+  }
 
 }
 
